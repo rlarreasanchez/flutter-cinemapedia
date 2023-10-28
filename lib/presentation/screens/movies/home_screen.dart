5bd9b4411b185bc7562039a7b2e9d98e356c04cc
Rafael Larrea Sánchez <rlarreasanchez@gmail.com>
2023-10-28 10:18:04 +0200

    Se diseña el menú de navegación

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -12,9 +12,8 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Center(
-        child: _HomeView(),
-      ),
+      body: _HomeView(),
+      bottomNavigationBar: CustomBottomNavigation(),
     );
   }
 }
